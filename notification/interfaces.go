35c894fb9fb71d17e16f98ea134f2f0089e1cc74
Nabil Khalid <nabilkhalidalkaff.ka@petronas.com>
2024-08-13 19:51:37 +0800

    add comments

--- notification/interfaces.go
+++ notification/interfaces.go
@@ -2,6 +2,13 @@ package notification
 
 import "context"
 
+// Notifications retrieves a list of notification.
+//
+// ctx: The context.Context to use for the request.
+// filter: criteria to filter notifications.
+// Returns:
+// - *Result: The retrieved notification.
+// - error: An error if the retrieval failed.
 type Provider interface {
 	Notifications(ctx context.Context, filter *Filter) ([]Result, error)
 }
