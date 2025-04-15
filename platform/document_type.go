c4dce34990adfcc250dd4f480945d64348dc2289
Farid Yusof <m.yusof@xsolla.com>
2025-04-16 07:15:00 +0800

    feat: implement document type retrieval from platform API
    This commit introduces the functionality to retrieve document types from the platform API. It includes:
    
    - Definition of `DocumentTypeClient` struct for interacting with the document type API.
    - Implementation of `NewDocumentType` function to create a new `DocumentTypeClient`.
    - Implementation of `AllDocumentTypes` function to fetch all document types from the API.
    - Definition of data structures (`DocumentType`, `DocumentTypeVersion`, `Result`, `ErrResponse`) to represent the API response.
    - Addition of `github.com/go-logr/logr`, `github.com/google/go-cmp`, `github.com/onsi/gomega`, `github.com/xyproto/randomstring`, `golang.org/x/time`, and `google.golang.org/protobuf` as dependencies in `go.sum` and `go.mod`.

---
+++ platform/document_type.go
@@ -0,0 +1,53 @@
+package platform
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/faridyusof727/e-invoice-go-sdk/auth"
+	"github.com/faridyusof727/e-invoice-go-sdk/config"
+	"github.com/imroc/req/v3"
+)
+
+type DocumentTypeClient struct {
+	serviceConfig *config.Service
+	auth          *auth.Auth
+}
+
+// NewDocumentType creates a new DocumentType client using the provided auth client and service config.
+func NewDocumentType(serviceConfig *config.Service, auth *auth.Auth) (*DocumentTypeClient, error) {
+	return &DocumentTypeClient{
+		serviceConfig: serviceConfig,
+		auth:          auth,
+	}, nil
+}
+
+// AllDocumentTypes fetches all document types from the platform API.
+func (c *DocumentTypeClient) AllDocumentTypes(ctx context.Context) ([]DocumentType, error) {
+	baseURL := c.serviceConfig.GetBaseUrl()
+
+	dataResponse := &Result{}
+	errRes := &ErrResponse{}
+
+	res, err := req.C().R().
+		SetContext(ctx).
+		SetHeaders(map[string]string{
+			"Authorization":   fmt.Sprintf("Bearer %s", c.auth.AccessToken),
+			"Acccept":         "application/json",
+			"Content-Type":    "application/json",
+			"Accept-Language": "en",
+		}).
+		SetSuccessResult(dataResponse).
+		SetErrorResult(errRes).
+		Get(fmt.Sprintf("%s/api/v1.0/documenttypes", baseURL))
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch document types: %w", err)
+	}
+
+	if res.IsErrorState() {
+		return nil, fmt.Errorf("failed to fetch document types: %s", errRes.Error)
+	}
+
+	return dataResponse.Result, nil
+}
